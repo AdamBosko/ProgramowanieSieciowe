ff02216148628e2b416de21e42ebfb12eb39c43f
Zacniewski Artur <zacniewski@PRACOWNIA.LOCAL>
2018-04-08 10:32:15 +0200

    Przygotowanie do Heroku

--- Hello.py
+++ Hello.py
@@ -5,5 +5,21 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 	
+@app.route("/test")
+def test():
+    return "Test/Hello_World/Test"
+
+@app.route("/suma")
+def suma():
+    a = 5
+    b = 7
+    return str(a + b)
+	
+@app.route("/iloczyn")
+def iloczyn():
+    a = 10
+    b = 2
+    return str(a * b)
+	
 if __name__ == '__main__':
     app.run()
